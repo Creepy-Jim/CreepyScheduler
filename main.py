971e50e5c224d0e242767aaca4d481c5fa210c01
Jim Huang <huangqi416@163.com>
2021-01-13 12:00:45 +0800

    Removed several features under developing

--- main.py
+++ main.py
@@ -1,131 +1,67 @@
 import sys
 import os
-import configparser
-# def nociter(currentmode)
-# to notify user that they are writing files/databases,
-# may used for nofitying users about the typ of databases or.ini files later
 
 # You may notice that now the method for handling file names' messy
 # --I will use .ini config in next ver
 # And the func for checking the presence of files
-# need to be caegorised in to one func, I think.
+# need to be categorised in to one func, I think.
 
 # Arg_handle: the argument handle. For detecting the length of the arguments,
 # and calling necessary functions, if specified in the parameters.
 
+# In this version, your database name must be 'CDB.txt', as I'm lazy...
 
-class arg_handle():
-    def arg_err(self, arg):
-        arglength = len(arg)
-        if arglength == 1:
-            print("Parameter Error, see if the following helps: \n")
-            help()
-            exit()
-        pass
 
-    def argchecker(self, arg):
-        print(arg)
+def arg_err(arg):
+    print("Parameter Error, see if the following helps: \n")
+    help()
+    exit()
+    pass
+
+# May be I should refine the arg_err checker? 
+
+def argchecker(arg):
+    for counter in arg:
         if arg == 'help':
             help()
         elif arg == 'form':
             help()
+        elif arg == '--dir':
+            os.chdir(arg[counter+1])
+#        elif arg == 'add':
+#            arg[counter+1]
+#        elif arg == 'finish':
+#            arg[counter+1]
+#        elif arg == 'list':
 
-# process_initialize: Something to do before starting the program.
-# this part may be removed as I'm trying to directly induce the
-# file directory by using parameters.
 
-
-class process_initialize():
-    def inireader(self):
-        
-
-    # read .ini files, by a standard, if exists.
-    # use configparser
-
-    def iniasker(self):
-        loopindicator = True
-        iniask = str(input(".ini config file not exists. Would you"
-                           "like to create a new one? (y/n)"))
-        while loopindicator = True:
-            if iniask == 'Yes' or iniask == 'y':
-                try:
-                    filecreator('creesche_config.ini')
-                    
-            elif iniask == 'No' or iniask == 'n':
-                print("As the configration file not found, "
-                      "please use the software under non-config file"
-                      " mode. (csche noini <filename>). Refer to helper files"
-                      "for further information."
-                exit()
-            else:
-                print("Please type either Yes/No or y/n.")
-
-    def tempfileattacher(self):
-        print("The current working directory is: ", os.getcwd())
-        filename = str(input("Input the name of database: (.txt)"))
-        # currently support txt files only, may change to sqlite
-        return filename
-        pass
-    # 'attach' files, if the .ini file DNE, unless user specified.
-
+##file attachment
+def tempfileattacher():
+    print("The current working directory is: ", os.getcwd())
+    # currently support txt files only, may change to sqlite
+    pass
 
 # file_operation: Hand over requests and write or read data
 # according to the requests.
 
-class config():
-    def ini_config(self,filename):
+def filereader(filename):
+    fileread = open(filename, "r")
+    for counter in fileread:
+        print(counter.strip())
+    fileread.close()
 
+def filewriter(filename, operation_name):
+    filewrite = open(filename, "a")
+    filewrite.close()
 
-class file_operation():
-    def filereader(self, filename):
-        print("reading files")
-        fileread = open(filename, "r")
-        for counter in fileread:
-            print(counter.strip())
-        fileread.close()
+def filecreator(filename):  # Create file if user necessary:
+    filecreate = open(filename, "x")
+    filecreate.close()
+    print("File", filename, "created.")
 
-    def filewriter(self, filename, operation_name):
-        filewrite = open(filename, "a")
-        print("Writing ")
-        filewrite.close()
-
-
-# filecheck: To check the validity of the files. Currently it has little use.
-
-
-class filecheck():
-    def filelengthchecker(self, filename):
-        try:
-            file_oper.fileread = open(filename, "r")
-        except(Exception):
-            exc.exception_handle(sys.exc_info[0])
-
-
-def filecreator():  # Create file if user necessary:
-    while(1):
-        userinput = input('Would you like to create a new file?(Yes/No)')
-        if userinput == "Yes" or userinput == "yes":
-            file_c_indicator = 1
-            break
-        elif userinput == "No" or userinput == "no":
-            file_c_indicator = 0
-            break
-        else:
-            print("Please input Yes or No")
-    if file_c_indicator == 1:
-        try:
-            filename = str(input("Input the name of the file: "))
-            filecreate = open("x", filename)
-            print("File", filename, "created.")
-            filecreate.close()
-        except(Exception):
-            exc_pros.exception_handle(sys.exc_info)
-    else:
-        print("The file will not be created.")
-    pass
+#    except(Exception):
+#        exception_handle(sys.exc_info)
 # filecreator: To create files directly in the program.
-# It need to be improved and adapted.
-
 
 def help():
     try:
@@ -136,22 +72,14 @@ def help():
 # open a file and output all guts it have
 
 
-class exc():
-    def exception_handle(self, exc_code):
-        print("I'm sorry, but there is a problem: ", exc_code)
-        exit()
+def exception_handle(exc_code):
+    print("I'm sorry, but there is a problem: ", exc_code)
+    exit()
 
-
-# Defining for each function
-arg = sys.argv
-arg_oper = arg_handle()
-file_oper = file_operation()
-# pros=process_initialize()
-exc_pros = exc()
-
-
-arg_oper.argchecker(arg)
-arg_oper.argchecker(arg[1])
-'''
-database_name=pros.tempfileattacher()
-filereader(database_name)'''
+#Major part of the program
+print('Welcome to the Creepy Scheduler 0.0.1, by Jim.')
+fname = "CDB.txt"
+if (os.path.exists(fname)) == False:
+    filecreator(fname)
+tempfileattacher()
+argchecker(sys.argv)
